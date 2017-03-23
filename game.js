2eb18d05fc4d67726325d71589f86a8f0e44679f
Xuanson On <hello@xuansonon.com>
2017-03-24 08:57:15 +1100

    update game.js for more functionality
    Includes auto earning, buy function, calculating cost for each asset.

--- game.js
+++ game.js
@@ -15,15 +15,15 @@ var clickerGame = {
   },
   "progression": {
     "manualClickValue": 1,
-    "autoClickValue":   0
+    "autoClickValue":   0,
   },
   "settings": {
     "assets": [
-      { "assetName": "Asset01", "assetCost": 30,  "assetEarning": 1 },
-      { "assetName": "Asset02", "assetCost": 50,  "assetEarning": 5 },
-      { "assetName": "Asset03", "assetCost": 150, "assetEarning": 9 },
-      { "assetName": "Asset04", "assetCost": 350, "assetEarning": 15 },
-      { "assetName": "Asset05", "assetCost": 950, "assetEarning": 25 }
+      { "assetName": "Asset00", "assetCost": 30,  "assetEarning": 1 },
+      { "assetName": "Asset01", "assetCost": 50,  "assetEarning": 5 },
+      { "assetName": "Asset02", "assetCost": 150, "assetEarning": 9 },
+      { "assetName": "Asset03", "assetCost": 350, "assetEarning": 15 },
+      { "assetName": "Asset04", "assetCost": 950, "assetEarning": 25 }
     ],
     "achievements": [
       { "achievementName": "Achievement01", "requirementdesc": "desc01", "reward": 100 },
@@ -36,31 +36,52 @@ var clickerGame = {
       { "achievementName": "Achievement08", "requirementdesc": "desc08", "reward": 800 },
       { "achievementName": "Achievement09", "requirementdesc": "desc09", "reward": 900 },
       { "achievementName": "Achievement10", "requirementdesc": "desc10", "reward": 1000 }
-    ]
+    ],
+    "version":"1.0.0b",
+    "developer": {
+      "name":"Xuanson On",
+      "email":"hello@xuansonon.com",
+      "github":"xuansonon",
+      "site":"www.xuansonon.com"
+    }
   },
 
   //Functions
   getTotalInArray: function(aArray) {
     var totalResult = 0;
     for(var i = 0; i < aArray.length; i++) {
-      if(aArray[i] != 0) totalResult++;
+      totalResult += aArray[i];
     }
     return totalResult;
   },
 
+  // minifyNumbers: function(value) {
+  //   if(value > 10000) {
+  //     conso
+  //   }
+  // }
+
   updateStats: function() {
-    currentMoneyText.innerHTML = this.userstats.currentMoney;
-    lifetimeEarningText.innerHTML = this.userstats.lifetimesavings;
+    currentMoneyText.innerHTML = (this.userstats.currentMoney).toFixed(2);
+    lifetimeEarningText.innerHTML = (this.userstats.lifetimesavings).toFixed(2);
     EPCText.innerHTML = this.progression.manualClickValue;
     EPSText.innerHTML = (this.progression.autoClickValue).toFixed(2);
-    totalAssetsText.innerHTML = this.getTotalInArray(this.userstats.assetsOwned);
     totalAchievementText.innerHTML = this.getTotalInArray(this.userstats.achievements);
     this.checkAvailableAssets();
+    this.updateAssets();
+  },
+
+  updateAssets: function() {
+    totalAssetsText.innerHTML = this.getTotalInArray(this.userstats.assetsOwned);
+    for(var i = 0; i < this.settings.assets.length; i++) {
+      document.getElementById("assetspan0" + i).innerHTML = this.userstats.assetsOwned[i];
+      document.getElementById("assetbtn0" + i).innerHTML = this.settings.assets[i].assetName + " (Cost: $" + (this.settings.assets[i].assetCost).toFixed(2) +")";
+    }
   },
 
   checkAvailableAssets: function() {
     for(var i = 0; i < (this.settings.assets).length; i++) {
-      var assetButton = document.getElementById("asset0" + i);
+      var assetButton = document.getElementById("assetbtn0" + i);
       if(this.userstats.currentMoney < this.settings.assets[i].assetCost) {
         assetButton.disabled = true;
       } else {
@@ -71,16 +92,10 @@ var clickerGame = {
 
   populateAssetsDiv: function() {
     for(var i = 0; i < (this.settings.assets).length; i++) {
-      var assetButton = document.createElement("button");
-      assetButton.id = "asset0" + i;
-      assetButton.onclick = function(){
-        var index = i;
-        return clickerGame.buyAsset(index);
-      };
-      var assetName = document.createTextNode(this.settings.assets[i].assetName);
-      assetButton.appendChild(assetName);
+      var assetLine = document.createElement("p");
+      assetLine.innerHTML += '<button id="assetbtn0'+ i +'" onclick="clickerGame.buyAsset('+i+')">' + this.settings.assets[i].assetName + " (Cost: $" + this.settings.assets[i].assetCost +")" + '</button> (Currently owned: <span id="assetspan0'+i+'"></span>)';
       var location = document.getElementById("shop");
-      location.appendChild(assetButton);
+      location.appendChild(assetLine);
     }
   },
 
@@ -94,11 +109,29 @@ var clickerGame = {
     }
   },
 
-  initGame: function() {
+  startAutoEarning: function() {
+    window.setInterval(function(){
+      clickerGame.userstats.currentMoney += (clickerGame.progression.autoClickValue)/235;
+      clickerGame.userstats.lifetimesavings += (clickerGame.progression.autoClickValue)/235;
+      clickerGame.updateStats();
+    }, 1);
+  },
+
+  init: function() {
+    currentMoneyText.innerHTML = this.userstats.currentMoney;
+    lifetimeEarningText.innerHTML = this.userstats.lifetimesavings;
+    EPCText.innerHTML = this.progression.manualClickValue;
+    EPSText.innerHTML = (this.progression.autoClickValue).toFixed(2);
+    totalAssetsText.innerHTML = this.getTotalInArray(this.userstats.assetsOwned);
+    totalAchievementText.innerHTML = this.getTotalInArray(this.userstats.achievements);
     this.populateAssetsDiv();
     this.populateAchievementsDiv();
+  },
+
+  initGame: function() {
+    this.init();
+    this.startAutoEarning();
     this.updateStats();
-    //this.startAutoEarning();
   },
 
   manualClick: function() {
@@ -107,24 +140,34 @@ var clickerGame = {
     this.updateStats();
   },
 
-  startAutoEarning: function() {
-    window.setInterval(function(){
-      this.userstats.currentMoney += this.progression.autoClickValue;
-      this.userstats.lifetimesavings += this.progression.autoClickValue;
-      this.updateStats();
-    }, 1000);
-  },
-
   buyAsset: function(buildingID) {
-    if(this.userstats.currentMoney >= this.settings.assets[buildingID].assetCost) {
-      this.userstats.currentMoney -= this.settings.assets[buildingID].assetCost;
-      this.userstats.autoClickValue += this.settings.assets[buildingID].assetEarning;
+    var ID = buildingID;
+    if(this.userstats.currentMoney >= this.settings.assets[ID].assetCost) {
+      this.userstats.currentMoney -= this.settings.assets[ID].assetCost;
+      this.userstats.assetsOwned[ID]++;
+      this.progression.autoClickValue += this.settings.assets[ID].assetEarning;
       //Increase asset cost - Log(n) + x
-      this.userstats.assetsOwned[buildingID] += 1;
+      this.settings.assets[buildingID].assetCost *= 1.17;
       this.updateStats();
     }
-  }
+  },
 
+  //Experimental Export - BETA
+  exportGame: function() {
+    var exportString = "";
+    exportString += this.userstats.currentMoney + "||" + this.userstats.lifetimesavings + "||";
+    for(var i = 0; i < this.userstats.assetsOwned.length; i++) {
+      exportString += this.userstats.assetsOwned[i];
+      if(i != (this.userstats.assetsOwned.length - 1)) exportString += ",";
+    }
+    exportString += "||";
+    for(var i = 0; i < this.userstats.achievements.length; i++) {
+      exportString += this.userstats.achievements[i];
+      if(i != (this.userstats.achievements.length - 1)) exportString += ",";
+    }
+    exportString += "||" + this.progression.manualClickValue + "||" + this.progression.autoClickValue;
+    console.log(btoa(exportString));
+  }
 }
 
 clickerGame.initGame();
